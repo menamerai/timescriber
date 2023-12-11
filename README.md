5e52663f6be8cfb556775665ca651cab1a676c91
menamerai <rphananh@gmail.com>
2023-12-11 13:35:24 -0500

    finishing touch to prototype

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# mi-transcript
- 
+# Timescribe
+
+## Installation Guide
+
+After downloading the repo, create a virtual environment. This is so that the installations do not conflict with your base Python interpreter. I usually run:
+
+```
+python -m venv env
+```
+
+But you can use other tools like conda for the job. Once the virtual environment is created, activate it. In windows, you do:
+
+```
+.\env\Scripts\activate
+```
+
+and in Linux, you do:
+
+```
+source ./env/bin/activate
+```
+
+I don't use Mac, so I don't know how to do it in Mac, but there should be tutorials.
+
+After activating the virtual environment, you are good to start installing the necessary libraries with:
+
+```
+pip install -r requirements.txt
+```
+
+This should install everything you need. To enable CUDA and run the model on your GPU however, you might need to reinstall torch with the CUDA toolkit, as I'm not sure if that is retained inside the requirements.txt
+
+You can do it with this command:
+
+```
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+```
+
+Or just use the [official Pytorch installation](https://pytorch.org/get-started/locally/) for your specific CUDA version. After this, you should be good to go!
+
+## Usage
+
+For now, the script can be run by executing the `main.py` file. I do have plan on turning this into a CLI application, so I will update the guide when I do so.
--- timescribe.py
+++ timescribe.py
@@ -1,18 +1,21 @@
-import json
 import os
+import torch
 import whisper_timestamped as whisper
 
 
 class TimeScribe:
-    def __init__(self, input_path, output_path):
-        self.input_path = input_path
-        self.output_path = output_path
+    def __init__(self, model_size: str = "medium"):
 
-    def transcribe_folder(self, input_path=None, output_path=None, model_size="medium") -> None:
-        if input_path == None and output_path == None:
-            input_path = self.input_path
-            output_path = self.output_path
+        if torch.cuda.is_available():
+            self.device = "cuda"
+            print("Using GPU")
+        else:
+            self.device = "cpu"
+            print("Using CPU")
 
+        self.model = whisper.load_model(model_size, self.device)
+
+    def transcribe_folder(self, input_path: str, output_path: str = "./output/transcript") -> None:
         # if output path doesn't exist, create it
         if not os.path.exists(output_path):
             os.makedirs(output_path)
@@ -20,7 +23,8 @@ class TimeScribe:
         # get all files in input path
         files = os.listdir(input_path)
 
-        speaker_segments = dict()
+        # speaker_segments = dict()
+        transcript = list()
 
         # iterate through files
         for file in files:
@@ -31,18 +35,26 @@ class TimeScribe:
             elif file.endswith(".wav") or file.endswith(".flac"):
                 # self.transcribe_file(os.path.join(input_path, file), os.path.join(output_path, file))
                 audio = whisper.load_audio(os.path.join(input_path, file))
-                model = whisper.load_model(model_size)
-                result = whisper.transcribe(model, audio, language="en", vad=True)
-                speaker_segments[file] = result["segments"]
+                result = whisper.transcribe(self.model, audio, language="en", vad=True)
+                # speaker_segments[file] = result["segments"]
+                for segment in result["segments"]:
+                    transcript.append((float(segment["start"]), file, segment["text"]))
 
             # otherwise, ignore it
             else:
                 print(f"Skipping {file}")
 
         
-        with open(f"{output_path}/data.json", "w") as f:
-            f.write(json.dumps(speaker_segments, indent=4, ensure_ascii=False))
+        # with open(f"{output_path}/data.json", "w") as f:
+        #     f.write(json.dumps(speaker_segments, indent=4, ensure_ascii=False))
+
+        transcript.sort(key=lambda a: a[0])
+        # write transcript to file in sorted order with timestamps
+        with open(f"{output_path}/transcript.txt", "w") as f:
+            for segment in transcript:
+                f.write(f"[{segment[0]}] {segment[1]}: {segment[2]}\n")
 
 if __name__ == "__main__":
-    timescribe = TimeScribe("./input", "./output")
-    timescribe.transcribe_folder(model_size="tiny")
+    
+    timescribe = TimeScribe()
+    timescribe.transcribe_folder("./input")
